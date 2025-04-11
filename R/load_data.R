736081a87b781107af6d6fd714622df4ef86b84b
Miguel Ayala (ESC) <miguel.ayala@esc.vic.gov.au>
2025-04-11 10:57:26 +1000

    ignore data

--- R/load_data.R
+++ R/load_data.R
@@ -1,23 +1,11 @@
 
 
-#### READ VEC DATA ####
-
-
-##### PREAMBLE #####
-
-library(readxl)
-library(lubridate)
-library(stringr)
-library(tidyverse)
-library(qs)
-library(usethis)
-
 # function to load data #
 
 loadtabs <- function(){
-  
+
   dbs <- qs::qread(file = "data/energydash_data.qs")
-  
+
   return(dbs)
 }
 
